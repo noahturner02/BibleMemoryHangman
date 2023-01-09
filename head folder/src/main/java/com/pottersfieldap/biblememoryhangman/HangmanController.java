838a7b50f44bea87be54b21eaa196631c2656721
noahturner02 <noahturner02@gmail.com>
2023-01-09 15:41:21 -0600

    Get the verse text to be displayed

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HangmanController.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HangmanController.java
@@ -1,9 +1,25 @@
 package com.pottersfieldap.biblememoryhangman;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
 
 public class HangmanController {
-    Pane manPane;
-    Pane verseTextPane;
+    @FXML
+    StackPane manPane;
+    @FXML
+    StackPane verseTextPane;
+
+    private void createBlanksFromText(String verseText) { // Turns the verse text into a set of blanks
+        Label l = new Label(verseText);
+        verseTextPane.getChildren().add(l);
+    }
+    @FXML
+    public void initialize() {
+        createBlanksFromText("Paul a servant of Christ Jesus, called to be an apostle, set apart for the gospel of God");
+    }
 }
 
---
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/VerseProcessing.java
@@ -0,0 +1,5 @@
+package com.pottersfieldap.biblememoryhangman;
+
+public class VerseProcessing {
+
+}
