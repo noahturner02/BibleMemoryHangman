75b5b37a86e589c300e1cbd97ab3e053aa24d755
noahturner02 <noahturner02@gmail.com>
2023-01-10 14:05:40 -0600

    Create Blanks From Text
    A new method in the HangmanController class adds the blanks for the verse text to the pane.

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HangmanController.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HangmanController.java
@@ -5,19 +5,31 @@ import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
 
 import java.util.List;
 
 
 public class HangmanController {
     @FXML
-    StackPane manPane;
+    Pane manPane;
     @FXML
-    StackPane verseTextPane;
+    Pane verseTextPane;
+    int LETTERWIDTH = 10;
 
     private void createBlanksFromText(String verseText) { // Turns the verse text into a set of blanks
         List<Word> wordList = VerseProcessing.textToWords("Paul a servant of Christ Jesus, called to be an apostle, set apart for the gospel of God");
-        System.out.println(wordList.get(4).getSize());
+        int xLayoutTracker = 0; // Tracks the blank lines that are drawn in the verse text pane. follows from left to right
+        int yLayoutTracker = 0; // Tracks the blank lines on the y axis
+        int lineSize = 0;
+        for (Word w : wordList) { // Make a new line for each word
+            Line l = new Line();
+            lineSize = LETTERWIDTH * w.getSize();
+            l.setStartX(xLayoutTracker);
+            l.setEndX(xLayoutTracker + lineSize);
+            xLayoutTracker += lineSize + LETTERWIDTH;
+            verseTextPane.getChildren().add(l);
+        }
     }
     @FXML
     public void initialize() {
