a8879f2be73b017969b2de7dd99558449e69bb66
noahturner02 <noahturner02@gmail.com>
2023-01-11 13:21:46 -0600

    Change function name. Add comments

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HangmanController.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HangmanController.java
@@ -18,22 +18,22 @@ public class HangmanController {
     TilePane verseTextPane;
     int LETTERWIDTH = 10;
 
-    private void createBlanksFromText(String verseText) { // Turns the verse text into a set of blanks
+    private void placeVerseText(String verseText) { // Places the invisible labels and shows the blanks of corresponding size
         List<Word> wordList = VerseProcessing.textToWords(verseText);
         int xLayoutTracker = 0; // Tracks the blank lines that are drawn in the verse text pane. follows from left to right
         int yLayoutTracker = 0; // Tracks the blank lines on the y axis
         int lineSize = 0;
         for (Word w : wordList) { // Make a new line for each word
-            Pane p = new Pane();
+            Pane p = new Pane(); // wrapper pane for the labels
             Label l = new Label(w.getText());
             p.getChildren().add(l);
-            l.layoutXProperty().bind(p.widthProperty().subtract(l.widthProperty()).divide(2));
+            l.layoutXProperty().bind(p.widthProperty().subtract(l.widthProperty()).divide(2)); // bind the label to the center of the pane
             verseTextPane.getChildren().add(p);
         }
     }
     @FXML
     public void initialize() {
-        createBlanksFromText("Paul a servant of Christ Jesus, called to be an apostle, set apart for the gospel of God, which he promised beforehand through his prophets in the holy scriptures, concerning his son, who was descended from David according to the flesh and was declared to be the son of God in power according to the Spirit of holiness by his resurrection from the dead, Christ Jesus our Lord.");
+        placeVerseText("Paul a servant of Christ Jesus, called to be an apostle, set apart for the gospel of God, which he promised beforehand through his prophets in the holy scriptures, concerning his son, who was descended from David according to the flesh and was declared to be the son of God in power according to the Spirit of holiness by his resurrection from the dead, Christ Jesus our Lord.");
     }
 }
 
