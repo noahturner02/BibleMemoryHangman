8fe856a07e0d50170b1f0f2556571db1fcd13098
noahturner02 <noahturner02@gmail.com>
2023-02-10 15:48:51 -0600

    Add needed fxids to the controller class

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/GameSetupController.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/GameSetupController.java
@@ -1,4 +1,24 @@
 package com.pottersfieldap.biblememoryhangman;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class GameSetupController {
+    @FXML
+    Button playButton;
+    @FXML
+    TextField verseTextField;
+    @FXML
+    ChoiceBox bookChoiceBox;
+    @FXML
+    TextField chapterField;
+    @FXML
+    TextField startVerseField;
+    @FXML
+    TextField endVerseField;
+
+    
+
 }
