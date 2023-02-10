ee25f7dc122aa3057ede0b398f749815c3da00bc
noahturner02 <noahturner02@gmail.com>
2023-02-10 17:37:30 -0600

    Play Button Does nothing when form incomplete

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/GameSetupController.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/GameSetupController.java
@@ -1,15 +1,18 @@
 package com.pottersfieldap.biblememoryhangman;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class GameSetupController {
     @FXML
     Button playButton;
     @FXML
-    TextField verseTextField;
+    TextArea verseTextField;
     @FXML
     ChoiceBox bookChoiceBox;
     @FXML
@@ -19,6 +22,23 @@ public class GameSetupController {
     @FXML
     TextField endVerseField;
 
-    
+    private void playButtonClicked() {
+        if (verseTextField.getText().isEmpty() || chapterField.getText().isEmpty() || startVerseField.getText().isEmpty() || !bookChoiceBox.isShowing()) {
+            System.out.println("Missing info. Please enter the text and the reference for your verse");
+        }
+        else {
+            System.out.println("Let the games begin!");
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        playButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                playButtonClicked();
+            }
+        });
+    }
 
 }
