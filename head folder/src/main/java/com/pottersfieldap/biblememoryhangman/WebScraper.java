ae615017d9774af55de8a671bbce9747b9d0baa3
noahturner02 <noahturner02@gmail.com>
2023-02-16 13:33:08 -0600

    Get verse text from the site
    Able to return some raw verse text. I think I will have to parse and filter out some verse numbers but, it should be fine

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/WebScraper.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/WebScraper.java
@@ -1,8 +1,11 @@
 package com.pottersfieldap.biblememoryhangman;
 import com.gargoylesoftware.htmlunit.*;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
+
+import java.util.List;
 
 import java.io.IOException;
+
 
 public class WebScraper {
     public static void testBrowse() {
@@ -12,8 +15,15 @@ public class WebScraper {
         wc.getOptions().setThrowExceptionOnScriptError(false);
         wc.getOptions().setPrintContentOnFailingStatusCode(false);
         try {
-            HtmlPage page  = wc.getPage("https://www.biblegateway.com/passage/?search=Romans%201&version=ESV");
+            HtmlPage page  = wc.getPage("https://www.biblegateway.com/passage/?search=Romans%203&version=ESV");
             System.out.println(page.getTitleText());
+            String xpath = "//div[@class='version-ESV result-text-style-normal text-html']";
+            List<HtmlDivision> divisionList = page.getByXPath(xpath);
+            HtmlDivision passageDiv = divisionList.get(0);
+            DomNodeList<HtmlElement> paragraphList = passageDiv.getElementsByTagName("p");
+            for (HtmlElement p : paragraphList) {
+                System.out.println(p.getTextContent());
+            }
             wc.getCurrentWindow().getJobManager().removeAllJobs();
             wc.close();
         } catch (IOException e) {
