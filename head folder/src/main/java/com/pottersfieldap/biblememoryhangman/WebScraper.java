50ede4e8bebb0781426bf1b937693299aa4c7233
noahturner02 <noahturner02@gmail.com>
2023-03-10 18:26:33 -0600

    Comment up the web scraper class.
    Also removed some print statements

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/WebScraper.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/WebScraper.java
@@ -10,23 +10,34 @@ import java.io.IOException;
 
 public class WebScraper {
     public static String getRawVerseText(String bookName, int chapterNum, int startVerse, int endVerse, String versionName) {
+        // Configure the web scraper.
         WebClient wc = new WebClient(BrowserVersion.CHROME);
+        // Options silence a lot of compiler messages
         wc.getOptions().setCssEnabled(false);
         wc.getOptions().setThrowExceptionOnFailingStatusCode(false);
         wc.getOptions().setThrowExceptionOnScriptError(false);
         wc.getOptions().setPrintContentOnFailingStatusCode(false);
         StringBuilder sb = new StringBuilder("");
+
         try {
+            // Navigate to the web page. Uses the function parameters to get the right URL
             HtmlPage page  = wc.getPage("https://www.biblegateway.com/passage/?search=" + bookName + chapterNum + "%3A" + startVerse + "-" + endVerse + "&version=" + versionName);
-            System.out.println(page.getTitleText());
+            // xpath puts us as the root which holds the passage text
             String xpath = "//div[@class='version-ESV result-text-style-normal text-html']";
             HtmlDivision passageDiv = page.getFirstByXPath(xpath);
+            // The only <p> tag in the div is the one that holds it all
             DomNodeList<HtmlElement> paragraphList = passageDiv.getElementsByTagName("p");
             HtmlElement passageParagraph = paragraphList.get(0);
 
+            /* Inside the <p> tags are a number of spans which have the text pieces out throughout them. We need to
+            iterate through and return anything that is just text
+             */
+            // For each span inside the <p> tag
             for (DomElement e : passageParagraph.getChildElements()) {
                 if (e.getTagName().equals("span")) {
+                    // For every node inside the span
                     for (DomNode n : e.getChildNodes()) {
+                        // If the node is text, append it to the sb
                         if (n.getNodeType() == Node.TEXT_NODE) {
                             sb.append(n.getTextContent());
                             sb.append(" ");
@@ -35,7 +46,7 @@ public class WebScraper {
                 }
             }
 
-            System.out.println(sb.toString());
+            // Close out the scraper
             wc.getCurrentWindow().getJobManager().removeAllJobs();
             wc.close();
         } catch (IOException e) {
