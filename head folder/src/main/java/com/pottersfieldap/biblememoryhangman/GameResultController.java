ced0b286afaeab2f06e3b470d4f930c1e1e5f68d
noahturner02 <noahturner02@gmail.com>
2023-03-14 20:13:03 -0500

    Make Game Result Window
    This window will display when the game ends, and will hopeful display the outcome of the game, an exhortation, and give the player the choice of playing again (returning the setup wizard) or quitting (exiting the game).
    
    Make a basic FXML file for the window, and a corresponding controller class with all the fields connected;

---
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/GameResultController.java
@@ -0,0 +1,16 @@
+package com.pottersfieldap.biblememoryhangman;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameResultController {
+    @FXML
+    Label gameStateText;
+    @FXML
+    Label exhortationText;
+    @FXML
+    Button replayButton;
+    @FXML
+    Button quitButton;
+}
