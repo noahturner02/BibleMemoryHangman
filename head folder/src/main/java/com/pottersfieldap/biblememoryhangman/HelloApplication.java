dd20ebabbac8290b49377dda768115082fffc6dd
noahturner02 <noahturner02@gmail.com>
2023-03-30 09:42:26 -0500

    Comment Up HelloApplication

--- head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HelloApplication.java
+++ head folder/src/main/java/com/pottersfieldap/biblememoryhangman/HelloApplication.java
@@ -11,9 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Call sceneRelay to add setup_stage to the stagemap for future reference
         SceneRelay sceneRelay = SceneRelay.getInstance();
         sceneRelay.addToStageMap("setup_stage", stage);
 
+        // Load the setup wizard to start the program
         FXMLLoader fxmlLoader= new FXMLLoader(HelloApplication.class.getResource("game-setup-wizard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 300);
         stage.setTitle("Bible Memory Hangman");
